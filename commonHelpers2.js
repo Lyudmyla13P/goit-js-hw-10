bbf064994b474550c9d76f3cc857744f802d2432
Lyudmyla13P <146709529+Lyudmyla13P@users.noreply.github.com>
2024-03-09 16:11:37 +0000

    Deploying to gh-pages from @ Lyudmyla13P/goit-js-hw-10@0e1454cbef2329dc317162bd6bc10be1da58de3a 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i}from"./assets/vendor-77e16229.js";const t=document.querySelector(".form");t.addEventListener("submit",l);function l(o){o.preventDefault();let s=t.elements.delay.value,m=t.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{m==="fulfilled"?e(`✅ Fulfilled promise in ${s}ms`):r(`❌ Rejected promise in ${s}ms`)},s)}).then(e=>{i.success({position:"topRight",message:e})}).catch(e=>{i.error({position:"topRight",message:e})}),t.reset()}
+//# sourceMappingURL=commonHelpers2.js.map
